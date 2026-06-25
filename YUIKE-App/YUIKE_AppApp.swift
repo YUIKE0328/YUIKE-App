53b729bda19c43047a14b10badf21f3fb0aba099
YUIKE0328 <kuniakiy.1128@gmail.com>
2026-06-25 19:39:40 +0900

    Initial Commit

---
+++ YUIKE-App/YUIKE_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YUIKE_AppApp.swift
+//  YUIKE-App
+//
+//  Created by Kuniaki Yui on 2026/06/25.
+//
+
+import SwiftUI
+
+@main
+struct YUIKE_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
